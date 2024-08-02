4b1152f91eb3330c49fdfb3b9d6441c3ef82265e
luisaferRP <luisaramiresporras103@gmail.com>
2024-08-02 08:12:50 -0500

    se crea la clase VisualInterfaz y se implementa validacion con try para la opcion ingresada

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
 ﻿
+
+using SimulacroPruebaC.models;
+
+do
+{
+    InterfazVisual.Visual();
+    int option;
+    try
+    {
+        bool opSuccess = int.TryParse(Console.ReadLine(),out option);
+    }
+    catch (Exception)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("¡Ey! ¡Por favor ingresa un dato valido!");
+        Console.ResetColor();
+    }
+
+
+    
+} while (true);
---
+++ models/InterfazVisual.cs
@@ -0,0 +1,19 @@
+namespace SimulacroPruebaC.models
+{
+    public class InterfazVisual
+    {
+        public static void Visual()
+        {
+            Console.WriteLine(@"
+            ======================================================
+                        Bienvenido usuario
+            ======================================================
+                1.Agregar un conductor.
+                2.Actualizar categoria de licensia.
+                3.Añadir años de experiencia.
+            ");
+
+        }
+    }
+}
+
